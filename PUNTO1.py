6c1e0a51134a6bbb3612de800b5a08be074e09da
SANJUANELO MOLINA ARLYS <asanjuan14@cuc.edu.co>
2022-03-02 19:52:44 -0500

    commit inicial

---
+++ PUNTO1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 19:21:16 2022
+
+@author: asanjuan14
+
+
+"""
+
+
+VALORDONADO = float (input('VALOR DONADO'))
+
+
+telecomunicaciones=VALORDONADO*0.10
+sistemas=VALORDONADO*0.25
+administracion=VALORDONADO*0.35
+sumadonacion=telecomunicaciones+sistemas+administracion
+contabilidad=VALORDONADO-sumadonacion
+
+print("valor donado",VALORDONADO)
+print("valor para el area de telecomunicaciones",telecomunicaciones)
+print("valor para el area de sistemas ",sistemas)
+print("valor para e area de administracion",administracion)
+print("valor para el area de contabilidad",contabilidad)
+
---
+++ PUNTO2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 19:42:20 2022
+
+@author: asanjuan14
+
+
+"""
+
+socio1 = float (input('inversion socio numero 1' ))
+socio2 = float (input('inversion socio numero 2 '))
+socio3 = float (input('inversion socio numero 3 '))
+
+
+suma=socio1+socio2+socio3
+
+s1=(socio1*100)/suma
+s2=(socio1*100)/suma
+s3=(socio1*100)/suma
+
+
+print("valor para el area de telecomunicaciones",telecomunicaciones)
+print("valor para el area de sistemas ",sistemas)
+print("valor para e area de administracion",administracion)
+print("valor para el area de contabilidad",contabilidad)
