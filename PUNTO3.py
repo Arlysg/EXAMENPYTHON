2a8297e682f1b74411b7186b878e1d17001ba38d
SANJUANELO MOLINA ARLYS <asanjuan14@cuc.edu.co>
2022-03-02 20:11:28 -0500

    punto 3

--- PUNTO3.py
+++ PUNTO3.py
@@ -5,3 +5,13 @@ Created on Wed Mar  2 19:56:18 2022
 @author: asanjuan14
 """
 
+sueldobase = float (input('ingrese sueldo BASE' ))
+ventasmes = float (input('valor de ventas en el mes' ))
+
+comision=ventasmes*0.15
+
+sueldototal=sueldobase+ventasmes
+
+print("valor de comision", comision)
+print("sueldo total ",sueldototal)
+
